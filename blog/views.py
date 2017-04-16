bb056c380d238c30ed3cc52c4f7121a64ae7d104
gokadi <gokadi@yandex.ru>
2017-04-16 04:10:09 +0300

    all dynamical

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from django.template import Context, RequestContext
 from django.template.context_processors import csrf
+from django.utils import translation
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from blog.models import Text
 from blog.text_analysis import init_backup
@@ -43,24 +44,24 @@ def contacts(request):
 def start(request):
     if request.method == 'GET':
         if Text.txt != '':
+            print('asdasda')
             context = {}
             context.update(csrf(request))
             analyser = init_backup.Analysis(Text.txt)
             # syntax_, water_, orthograf_, inform_, total_, tonal_ = init_backup.analysis(Text.txt)
             syntax_, water_, orthograf_, inform_, total_, tonal_ = analyser.analyse()
-            # context = RequestContext(request, {'Text': Text.txt, 'water': "%.2f" % water_, 'inform': "%.2f" % inform_,
-            #                                    'orthograf': "%.2f" % orthograf_, 'syntax': "%.2f" % syntax_,
-            #                                    'advert': "%.2f" % 5, 'tonal': "%.2f" % tonal_,
-            #                                    'total': "%.2f" % total_})
-            context = {'Text': Text.txt, 'water': "%.2f" % water_, 'inform': "%.2f" % inform_,
+            context = RequestContext(request, {'water': "%.2f" % water_, 'inform': "%.2f" % inform_,
                                                'orthograf': "%.2f" % orthograf_, 'syntax': "%.2f" % syntax_,
                                                'advert': "%.2f" % 5, 'tonal': "%.2f" % tonal_,
-                                               'total': "%.2f" % total_}
-            return JsonResponse(context)
-            # return render(request, "blog/home.html", context)
+                                               'total': "%.2f" % total_})
+            # context = {'Text': Text.txt, 'water': "%.2f" % water_, 'inform': "%.2f" % inform_,
+            #                                    'orthograf': "%.2f" % orthograf_, 'syntax': "%.2f" % syntax_,
+            #                                    'advert': "%.2f" % 5, 'tonal': "%.2f" % tonal_,
+            #                                    'total': "%.2f" % total_}
+            # return JsonResponse(context)
+            return render(request, "blog/table.html", context)
+            # return HttpResponse(context, content_type="text/html")
         else:
-            return JsonResponse({'Text': Text.txt})
-            # return render(request, "blog/home.html", {'Text': Text.txt})
-
-
-
+            # return JsonResponse({'Text': Text.txt})
+            return render(request, "blog/table.html")
+            # return HttpResponse(content_type="text/html")
